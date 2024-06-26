95ff2881cbf2307af0b9f7448b374ceff1a35b76
gllmsb <142408342+gllmsb@users.noreply.github.com>
2024-06-26 10:12:03 +0200

    General Typescript code, added mixins and changed the variables color wise, i have added a font and changed the button design.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,122 @@
 "use strict";
-var text = "Hello";
-console.log(text);
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var appElement = document.getElementById("app");
+var boardElement = document.getElementById("board");
+var ROW_COUNT = 3;
+var COL_COUNT = 3;
+var boardState = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+];
+var currentMove = "X";
+var winner = "";
+function createCell(row, col, content) {
+    if (content === void 0) { content = ""; }
+    var cell = document.createElement("button");
+    cell.setAttribute("data-row", row.toString());
+    cell.setAttribute("data-col", col.toString());
+    cell.setAttribute("data-content", content);
+    cell.classList.add("cell");
+    cell.addEventListener("click", function () {
+        if (winner)
+            return;
+        if (boardState[row][col] === "") {
+            boardState[row][col] = currentMove;
+            winner = checkBoard(currentMove);
+            if (!winner) {
+                currentMove = currentMove === "X" ? "O" : "X";
+            }
+            renderBoard();
+        }
+    });
+    return cell;
+}
+function flattenBoard(board) {
+    return board.reduce(function (acc, row) { return acc.concat(row); }, []);
+}
+function checkBoard(player) {
+    var flattenedBoard = flattenBoard(boardState);
+    if (flattenedBoard.filter(function (c) { return c; }).length === 9) {
+        return "Draw";
+    }
+    var horizontal = [0, 3, 6].map(function (c) { return [c, c + 1, c + 2]; });
+    var vertical = [0, 1, 2].map(function (c) { return [c, c + 3, c + 6]; });
+    var diagonal = [
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    var allWins = __spreadArray(__spreadArray(__spreadArray([], horizontal, true), vertical, true), diagonal, true);
+    var result = allWins.some(function (indices) {
+        return flattenedBoard[indices[0]] === player &&
+            flattenedBoard[indices[1]] === player &&
+            flattenedBoard[indices[2]] === player;
+    });
+    if (result)
+        return player;
+    return "";
+}
+function renderBoard() {
+    if (!appElement) {
+        throw new Error("Cannot find app element");
+    }
+    if (!boardElement) {
+        throw new Error("Cannot find board element");
+    }
+    boardElement.innerHTML = "";
+    for (var i = 0; i < ROW_COUNT; i++) {
+        for (var j = 0; j < COL_COUNT; j++) {
+            var cell = createCell(i, j, boardState[i][j]);
+            console.log("Appending cell at row ".concat(i, ", col ").concat(j));
+            boardElement.appendChild(cell);
+        }
+    }
+    var oldMoveElement = document.getElementById("move-element");
+    if (oldMoveElement) {
+        oldMoveElement.remove();
+    }
+    var moveElement = document.createElement("p");
+    moveElement.id = "move-element";
+    moveElement.innerText = winner
+        ? "Winner: ".concat(winner)
+        : "Next Move: ".concat(currentMove);
+    moveElement.classList.add("current-move");
+    var resetButton = document.getElementById("reset");
+    if (resetButton) {
+        appElement.insertBefore(moveElement, resetButton);
+    }
+    else {
+        appElement.appendChild(moveElement);
+    }
+}
+function init() {
+    var resetButton = document.getElementById("reset");
+    if (!resetButton) {
+        throw new Error("No Reset button");
+    }
+    resetButton.addEventListener("click", function () {
+        boardState = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ];
+        currentMove = "X";
+        winner = "";
+        renderBoard();
+    });
+    renderBoard();
+}
+if (appElement && boardElement) {
+    init();
+}
+else {
+    throw new Error("Required elements are missing in the DOM");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,122 @@
-const text = "Hello";
-console.log(text);
+const appElement = document.getElementById("app");
+const boardElement = document.getElementById("board");
+const ROW_COUNT = 3;
+const COL_COUNT = 3;
+
+type Cell = "X" | "O" | "";
+type Board = [[Cell, Cell, Cell], [Cell, Cell, Cell], [Cell, Cell, Cell]];
+
+let boardState: Board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+];
+
+let currentMove: "X" | "O" = "X";
+let winner: Cell | "Draw" = "";
+
+function createCell(row: number, col: number, content = ""): HTMLButtonElement {
+  const cell = document.createElement("button");
+  cell.setAttribute("data-row", row.toString());
+  cell.setAttribute("data-col", col.toString());
+  cell.setAttribute("data-content", content);
+  cell.classList.add("cell");
+  cell.addEventListener("click", () => {
+    if (winner) return;
+    if (boardState[row][col] === "") {
+      boardState[row][col] = currentMove;
+      winner = checkBoard(currentMove);
+
+      if (!winner) {
+        currentMove = currentMove === "X" ? "O" : "X";
+      }
+
+      renderBoard();
+    }
+  });
+  return cell;
+}
+
+function flattenBoard(board: Board): Cell[] {
+  return board.reduce((acc, row) => acc.concat(row), [] as Cell[]);
+}
+
+function checkBoard(player: Cell): Cell | "Draw" {
+  const flattenedBoard = flattenBoard(boardState);
+  if (flattenedBoard.filter((c) => c).length === 9) {
+    return "Draw";
+  }
+  const horizontal = [0, 3, 6].map((c) => [c, c + 1, c + 2]);
+  const vertical = [0, 1, 2].map((c) => [c, c + 3, c + 6]);
+  const diagonal = [
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const allWins = [...horizontal, ...vertical, ...diagonal];
+  let result = allWins.some(
+    (indices) =>
+      flattenedBoard[indices[0]] === player &&
+      flattenedBoard[indices[1]] === player &&
+      flattenedBoard[indices[2]] === player
+  );
+  if (result) return player;
+  return "";
+}
+
+function renderBoard() {
+  if (!appElement) {
+    throw new Error("Cannot find app element");
+  }
+  if (!boardElement) {
+    throw new Error("Cannot find board element");
+  }
+  boardElement.innerHTML = "";
+  for (let i = 0; i < ROW_COUNT; i++) {
+    for (let j = 0; j < COL_COUNT; j++) {
+      const cell = createCell(i, j, boardState[i][j]);
+      console.log(`Appending cell at row ${i}, col ${j}`);
+      boardElement.appendChild(cell);
+    }
+  }
+  const oldMoveElement = document.getElementById("move-element");
+  if (oldMoveElement) {
+    oldMoveElement.remove();
+  }
+  const moveElement = document.createElement("p");
+  moveElement.id = "move-element";
+  moveElement.innerText = winner
+    ? `Winner: ${winner}`
+    : `Next Move: ${currentMove}`;
+  moveElement.classList.add("current-move");
+  const resetButton = document.getElementById("reset");
+  if (resetButton) {
+    appElement.insertBefore(moveElement, resetButton);
+  } else {
+    appElement.appendChild(moveElement);
+  }
+}
+
+function init() {
+  const resetButton = document.getElementById("reset");
+  if (!resetButton) {
+    throw new Error("No Reset button");
+  }
+  resetButton.addEventListener("click", () => {
+    boardState = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
+    currentMove = "X";
+    winner = "";
+    renderBoard();
+  });
+  renderBoard();
+}
+
+if (appElement && boardElement) {
+  init();
+} else {
+  throw new Error("Required elements are missing in the DOM");
+}
